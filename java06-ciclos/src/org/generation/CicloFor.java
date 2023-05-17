3b7b4d3d27ba78438847fb229127d1936064d59c
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-17 10:41:12 -0600

    ci: Java 06 ciclo for

---
+++ java06-ciclos/src/org/generation/CicloFor.java
@@ -0,0 +1,30 @@
+package org.generation;
+
+public class CicloFor {
+
+	public static void main(String[] args) {
+		/**
+		 * Sintaxis:
+		 * 
+		 * for( expresion_inicial ; comparacion; expresion_final) instrucción;
+		 * 
+		 * for( expresion_inicial ; comparacion; expresion_final){ instrucciones; }
+		 * 
+		 */
+		
+		int j;
+		for (j = 1; j <= 5; ++j);
+		System.out.println("Hola " + j); // Hola 6
+		
+		// imprimir la tabla de multiplicar del 1 al 3.
+		/**
+		 *  1 * 1 = 1
+		 *  1 * 10 = 10
+		 *  2 * 1 = 2
+		 *  3 * 10 = 30
+		 */
+		
+
+	}
+
+}
