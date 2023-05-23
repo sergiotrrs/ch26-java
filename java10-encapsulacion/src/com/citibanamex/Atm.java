9e6e8eb3d9a8a787c76f6e784351e91bc23a3721
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-23 12:44:41 -0600

    ci: encapsulacion  de atributos

--- java10-encapsulacion/src/com/citibanamex/Atm.java
+++ java10-encapsulacion/src/com/citibanamex/Atm.java
@@ -1,16 +1,24 @@
 package com.citibanamex;
 
 public class Atm {
-	int serialNumber;
-	public double balance;
 	
-	public static int counterAtm = 1;
+	private int serialNumber;
+	private double balance;
+	
+	private static int counterAtm = 1;
 	
 	public Atm() {
-		
+		this.balance = 100_000;
+		this.serialNumber = counterAtm ++;
 	}
 	
 	public Atm(double balance){
+		this(); // se invoca al constructor sin parámetros (default)
 		this.balance = balance;
 	}
+	
+	public Atm(String balance) {
+		this( Double.parseDouble(balance)  );		
+	}
+		
 }
--- java10-encapsulacion/src/org/generation/test/AtmTest.java
+++ java10-encapsulacion/src/org/generation/test/AtmTest.java
@@ -7,16 +7,22 @@ public class AtmTest {
 	public static void main(String[] args) {
 		System.out.println("Counter Atm: " +  Atm.counterAtm );
 		
-		Atm cajeroA = new Atm(10_000);
-		// Al usar el constructor por default, se agregue
-		// 100_000 al atributo balance.
-		Atm cajeroB = new Atm();
-		
-		System.out.println( cajeroA.balance);
-		System.out.println( cajeroB.balance);
-		
+		Atm cajeroA = new Atm(9_000);
+		Atm cajeroB = new Atm();	
+		Atm cajeroC = new Atm("777000");
 	
+		System.out.println( cajeroA.serialNumber);
+		System.out.println( cajeroA.balance);
+		System.out.println( cajeroB.serialNumber);
+		System.out.println( cajeroB.balance);
+		System.out.println( cajeroC.serialNumber);
+		System.out.println( cajeroC.balance);
+				
+		cajeroA.balance = 1;
+		cajeroA.serialNumber = -1;
 		
+		System.out.println( cajeroA.serialNumber);
+		System.out.println( cajeroA.balance);
 
 	}
 
