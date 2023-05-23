24b87784b3049e6900be6ea58d919bf57220e9c9
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-23 13:07:22 -0600

    refactor: setters and getters

--- java10-encapsulacion/src/com/citibanamex/Atm.java
+++ java10-encapsulacion/src/com/citibanamex/Atm.java
@@ -8,17 +8,50 @@ public class Atm {
 	private static int counterAtm = 1;
 	
 	public Atm() {
-		this.balance = 100_000;
-		this.serialNumber = counterAtm ++;
+		setBalance(100_000);
+		setSerialNumber( counterAtm ++ );		
 	}
 	
 	public Atm(double balance){
 		this(); // se invoca al constructor sin parámetros (default)
-		this.balance = balance;
+		//this.balance = balance;
+		setBalance(balance);
 	}
 	
 	public Atm(String balance) {
 		this( Double.parseDouble(balance)  );		
+	}
+	
+	// Para acceder a los atributos encapsulados
+	// se usan los setters y getters.
+	
+	public int getSerialNumber() {		
+		return this.serialNumber;
+	}
+	
+	private int setSerialNumber( int serialNumber ) {
+		return this.serialNumber = serialNumber;
+	}
+	
+	public double getBalance() {
+		return this.balance;
+	}
+	
+	private double setBalance(double balance) {
+		return this.balance = balance;
+	}
+	
+	public static int getCounterAtm() {
+		return counterAtm;
+	}
+	
+	public double deposit(double amount) {
+		if(amount>0) {			
+			return setBalance( getBalance() + amount );
+		}
+		else {
+			return 0;
+		}			
 	}
 		
 }
--- java10-encapsulacion/src/org/generation/test/AtmTest.java
+++ java10-encapsulacion/src/org/generation/test/AtmTest.java
@@ -5,24 +5,13 @@ import com.citibanamex.Atm;
 public class AtmTest {
 
 	public static void main(String[] args) {
-		System.out.println("Counter Atm: " +  Atm.counterAtm );
+		System.out.println("Counter Atm: " +  Atm.getCounterAtm() );
 		
 		Atm cajeroA = new Atm(9_000);
 		Atm cajeroB = new Atm();	
 		Atm cajeroC = new Atm("777000");
 	
-		System.out.println( cajeroA.serialNumber);
-		System.out.println( cajeroA.balance);
-		System.out.println( cajeroB.serialNumber);
-		System.out.println( cajeroB.balance);
-		System.out.println( cajeroC.serialNumber);
-		System.out.println( cajeroC.balance);
-				
-		cajeroA.balance = 1;
-		cajeroA.serialNumber = -1;
 		
-		System.out.println( cajeroA.serialNumber);
-		System.out.println( cajeroA.balance);
 
 	}
 
