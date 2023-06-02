0003e47b4d6188a9559a796b68b587b15ad11ced
Sergio Torres <sergio.torres@idr.edu.mx>
2023-06-02 10:28:29 -0600

    ci: insert customers en import.sql

---
+++ express_railway/src/main/resources/import.sql
@@ -0,0 +1,18 @@
+
+
+INSERT INTO `customers`( `firstname`, `lastname`, `address`, `email`) VALUES( "Sergio", "Torres", "Av. siempre viva", "sergio@gmail.com");
+ 
+
+INSERT INTO `customers`( `firstname`, `lastname`, `address`, `email`) VALUES( "Alexis", "MT", "Av. Benito Juárez", "alexis@gmail.com");
+ 
+
+INSERT INTO `customers`( `firstname`, `lastname`, `address`, `email`) VALUES( "Berenice", "Gudiño", "Calle Las Palmas n.45", "berenice@gmail.com");
+
+
+INSERT INTO `customers`( `firstname`, `lastname`, `address`, `email`) VALUES( "Abraham", "Castillo", "Calle Hidalgo n.89", "abraham@gmail.com");
+
+
+INSERT INTO `customers`( `firstname`, `lastname`, `address`, `email`) VALUES( "Nicolas", "Walser", "4 Privet Drive", "nicolas@gmail.com");
+
+
+INSERT INTO `customers`( `firstname`, `lastname`, `address`, `email`) VALUES( "Jessica", "Sánchez", "Av. Las Águilas n.90", "jessica@gmail.com");
