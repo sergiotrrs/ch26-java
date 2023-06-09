8e27502ccf1046321400cfafaf54ff51f8cbad66
Sergio Torres <sergio.torres@idr.edu.mx>
2023-06-09 15:50:32 -0600

    ci: nuevos clientes en import.sql

--- ch26-lachiapaneca/src/main/resources/import.sql
+++ ch26-lachiapaneca/src/main/resources/import.sql
@@ -1,2 +1,5 @@
 
-INSERT INTO `customer`(`first_name`, `last_name`, `email`, `password`, `active`, `address`) VALUES ("Patricio", "Estrella", "estrella@gmail.com","123", 0, "Fondo de Vikini");
+INSERT INTO `customer`(`first_name`, `last_name`, `email`, `password`, `active`, `address`) VALUES ("Patricio", "Estrella", "estrella@gmail.com","123", 0, "Fondo de Vikini n.2");
+INSERT INTO `customer`(`first_name`, `last_name`, `email`, `password`, `active`, `address`) VALUES ("Bob", "Esponja", "bob@gmail.com","123", 1, "Fondo de Vikini n.1");
+INSERT INTO `customer`(`first_name`, `last_name`, `email`, `password`, `active`, `address`) VALUES ("Arenita", "Mejillas", "arenita@gmail.com","123", 1, "Fondo de Vikini n. 8");
+INSERT INTO `customer`(`first_name`, `last_name`, `email`, `password`, `active`, `address`) VALUES ("Calamardo", "Tentáculos", "calamardo@gmail.com","123", 0, "Fondo de Vikini n.3");
